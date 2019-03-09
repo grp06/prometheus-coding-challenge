3639d29561849f3bbba4c0b112923ce9f3408b77
George Pickett <gpickett00@gmail.com>
2019-03-09 13:13:28 -0500

    Updates

---
+++ precache-manifest.a53844349809640757f286c6c5a5f394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c4865530d945b2caf",
+    "url": "/prometheus-coding-challenge/static/css/main.4cd9240f.chunk.css"
+  },
+  {
+    "revision": "448c4865530d945b2caf",
+    "url": "/prometheus-coding-challenge/static/js/main.1abad3ab.chunk.js"
+  },
+  {
+    "revision": "b06e7a0866f53bddae2e",
+    "url": "/prometheus-coding-challenge/static/js/runtime~main.508d7fc1.js"
+  },
+  {
+    "revision": "5aef3292ef81e106dddb",
+    "url": "/prometheus-coding-challenge/static/js/2.50a451c8.chunk.js"
+  },
+  {
+    "revision": "c53c16eb01f461bee37342098e537e20",
+    "url": "/prometheus-coding-challenge/index.html"
+  }
+];
